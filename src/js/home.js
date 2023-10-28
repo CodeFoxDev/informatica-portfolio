bc2df4ba4da7b4aa6d54184bf5cd780eb7cb57a5
Codefoxdev <admin@codefoxdev.com>
2023-10-28 22:07:31 +0200

    Switched design to a completly new one

--- src/js/home.js
+++ src/js/home.js
@@ -1,34 +1,41 @@
 import config from "../site.config.js";
 
 (function () {
-  const title = document.querySelector("#title-changing");
+  const title = document.querySelector("#title-type");
 
   let index = 0;
+  let animationInProgress = false;
 
-  setInterval(() => {
+  setInterval(async () => {
+    if (animationInProgress) return;
     if (index < config.home.titles.length - 1) index++;
     else index = 0;
     const content = config.home.titles[index];
 
-    typeContent(title, content, 20);
+    animationInProgress = true;
+    await typeContent(title, content, 20);
+    animationInProgress = false;
   }, 5000);
 })();
 
 function typeContent(element, content, speed) {
-  let innerHTMLLength = element.innerHTML.length;
-  let totalLength = innerHTMLLength + content.length;
-  let i = 0;
-  let iv = setInterval(() => {
-    if (i < innerHTMLLength) {
-      // Remove char
-      element.innerHTML = element.innerHTML.slice(0, -1);
-    } else if (i < totalLength) {
-      // Add char
-      let contentIndex = i - innerHTMLLength;
-      element.innerHTML += content.charAt(contentIndex);
-    } else {
-      clearInterval(iv);
-    }
-    i++;
-  }, speed);
+  return new Promise(resolve => {
+    let innerHTMLLength = element.innerHTML.length;
+    let totalLength = innerHTMLLength + content.length;
+    let i = 0;
+    let iv = setInterval(() => {
+      if (i < innerHTMLLength) {
+        // Remove char
+        element.innerHTML = element.innerHTML.slice(0, -1);
+      } else if (i < totalLength) {
+        // Add char
+        let contentIndex = i - innerHTMLLength;
+        element.innerHTML += content.charAt(contentIndex);
+      } else {
+        clearInterval(iv);
+        resolve();
+      }
+      i++;
+    }, speed);
+  });
 }
--- src/main.js
+++ src/main.js
@@ -1,2 +1,29 @@
 import "./js/projects.js";
 import "./js/skills.js";
+
+const scrollShow = Array.from(document.querySelectorAll("[data-scroll-class]"));
+scrollShow?.forEach((e, i) => {
+  const attr = e.getAttribute("data-scroll-threshold");
+  const threshold = parseCssVal(attr);
+  if (typeof threshold != "number" || !attr) return;
+  addEventListener("scroll", () => {
+    const className = e.getAttribute("data-scroll-class");
+    if (window.scrollY < threshold) return e.classList.remove(className);
+    e.classList.add(className);
+  });
+});
+
+function parseCssVal(inp, toPX) {
+  const ex = /([0-9\.]*)([a-z]*)/gi.exec(inp);
+  const val = parseFloat(ex[1]);
+  const unit = ex[2];
+  if (!unit) return [ex[1], ex[2]];
+  if (unit == "vh") {
+    return (val / 100) * window.innerHeight;
+  } else if (unit == "vw") {
+    return (val / 100) * window.innerWidth;
+  } else if (unit == "rem") {
+    return val * 16;
+  }
+  return val;
+}
